37ff571b36ba2674aa74ee4f7acd5f9821300346
Brent Lawrence C. Bernardo <brentbernardo76@gmail.com>
2026-03-04 22:41:34 +0800

    Update main.dart

--- inaagapay_flutter_v2/lib/main.dart
+++ inaagapay_flutter_v2/lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://buvseyqcdacctlupznya.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1dnNleXFjZGFjY3RsdXB6bnlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MzE2NTUsImV4cCI6MjA4ODIwNzY1NX0.VPh8ZZFqdeFyb8YuMxllbJJa-nWl4VXNq74o6-Itjjw',
+  );
+
+  runApp(const InaagapayApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class InaagapayApp extends StatelessWidget {
+  const InaagapayApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Inaagapay Connected to Supabase',
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
     );
